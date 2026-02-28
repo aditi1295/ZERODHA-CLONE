a92ed60b308404f87530e71e0d69988dc0fe082e
12aditi95 <aditi1295porwal@gmail.com>
2026-02-28 22:49:13 +0530

    setup react-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import './index.css';
 import HomePage from './landing_page/home/HomePage';
+import SignUp from  './landing_page/signup/SignUp';
+import AboutPage from  './landing_page/about/AboutPage';
+import PrdouctPage  from  './landing_page/products/ProductPage'
+import SupportPage from  './landing_page/support/SupportPage'
+import Pricing from './landing_page/home/Pricing';
+import Navbar from './landing_page/Navbar';
+import Footer from './landing_page/Footer';
+import NotFound from './landing_page/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <HomePage/>
-  </React.StrictMode>
+  <BrowserRouter>
+  <Navbar/>
+  <Routes>
+    <Route path="/" element={<HomePage/>}/>
+    <Route path="/signup" element={<SignUp/>}/>
+    <Route path="/about" element={<AboutPage/>}/>
+    <Route path="/product" element={<PrdouctPage/>}/>
+    <Route path="/pricing" element={<Pricing/>}/>
+    <Route path="/support" element={<SupportPage/>}/>
+    <Route path="*" element={<NotFound/>}/> 
+  </Routes>
+  <Footer/>
+  </BrowserRouter>
 );
 
 
