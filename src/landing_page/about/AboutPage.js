7040ef59bb0bd7e3283fd0d01b15d50174fd57a4
12aditi95 <aditi1295porwal@gmail.com>
2026-03-01 22:05:28 +0530

    added about page

--- src/landing_page/about/AboutPage.js
+++ src/landing_page/about/AboutPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from './Hero';
+import Founder from './Founder';
 import Team from './Team';
 function AboutPage() {
     return ( 
         <>
         <Hero/>
+        <Founder/>
         <Team/>
         </>
      );
